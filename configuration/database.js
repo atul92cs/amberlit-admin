31350e08536cfe019849138013a7e23834bc0650
Atul <atul.11192@gmail.com>
2019-09-16 08:58:45 +0530

    making final changes

--- configuration/database.js
+++ configuration/database.js
@@ -1,6 +1,6 @@
 const Sequelize=require('sequelize');
-const sequelize=new Sequelize('ambelit','root','seed',{
-  host:'localhost',
+const sequelize=new Sequelize('I4gRGwXdhW','I4gRGwXdhW','cR4qpY4wfX',{
+  host:'remotemysql.com',
   port:'3306',
   dialect:'mysql'
 });
