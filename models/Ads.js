1fb39132e892711b1ac85db391ee174256b99da9
Atul kumar singh <atul.11192@gmail.com>
2019-09-30 17:31:54 +0530

    adding some more code

--- models/Ads.js
+++ models/Ads.js
@@ -2,13 +2,12 @@ const sequelize=require('sequelize');
 const db=require('../configuration/database');
 const Ads=db.define('ads',{
   id:{type:sequelize.INTEGER,primaryKey:true,autoIncrement:true},
-  Date:{type:sequelize.STRING},
   Title:{type:sequelize.STRING},
   Content:{type:sequelize.STRING},
   Picture:{type:sequelize.STRING},
   Category:{type:sequelize.INTEGER},
   Subcategory:{type:sequelize.INTEGER},
-  Userid:{type:sequelize.INTEGER},
+  Postedby:{type:sequelize.INTEGER},
   Status:{type:sequelize.STRING}
 });
 module.exports=Ads;
